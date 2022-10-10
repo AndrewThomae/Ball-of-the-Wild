09efd3fef691c2f8928f5e33f777719e16b9a64a
NSimon23 <82419865+NSimon23@users.noreply.github.com>
2022-10-10 17:23:25 -0400

    set default pawn to none

--- BallOfTheWild/Intermediate/Build/Win64/BallOfTheWildEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ BallOfTheWild/Intermediate/Build/Win64/BallOfTheWildEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.33.31629
-#include "C:/Users/alexb/Documents/GitHub/Ball-of-the-Wild/BallOfTheWild/Intermediate/Build/Win64/BallOfTheWildEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30146
+#include "C:/Users/simon/OneDrive/Documents/Unreal Projects/Ball-of-the-Wild/BallOfTheWild/Intermediate/Build/Win64/BallOfTheWildEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- BallOfTheWild/Source/BallOfTheWild/BallOfTheWildGameMode.cpp
+++ BallOfTheWild/Source/BallOfTheWild/BallOfTheWildGameMode.cpp
@@ -7,9 +7,10 @@
 ABallOfTheWildGameMode::ABallOfTheWildGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+		//DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
+	DefaultPawnClass = nullptr;
 }
