685c9dbe998d7213ad353a0e4ec7a6f3472be403
Wes Turnbull <70210626+wcturnbull@users.noreply.github.com>
2022-12-02 00:32:49 -0500

    Made several small fixes
    Stamina bar doesn't show in challenge mode, you can navigate to main menu from character select and character select from match settings. Also updated several things visually in a live match.

--- BallOfTheWild/start.bat
+++ BallOfTheWild/start.bat
@@ -1,1 +1,1 @@
-start "" "C:\Program Files\Epic Games\UE_5.0\Engine\Binaries\Win64\UnrealEditor.exe" "D:\UnrealEngine\Ball-of-the-Wild\BallOfTheWild\BallOfTheWild.uproject" -game -ResX=1280 -ResY=720 -WINDOWED -log
+start "" "C:\Program Files\Epic Games\UE_5.0\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\Wes Turnbull\Documents\GitHub\Ball-of-the-Wild\BallOfTheWild\BallOfTheWild.uproject" -game -ResX=1280 -ResY=720 -WINDOWED -log
